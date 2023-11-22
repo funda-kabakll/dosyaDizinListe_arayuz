5c6300f7811f13b9df0c40213e1685a203625697
Funda Kabaklı <funda_kabakli@hacettepe.edu.tr>
2023-11-22 21:27:59 +0300

    çalışır durumda kaydedildi

--- arayuz.py
+++ arayuz.py
@@ -5,33 +5,27 @@ import klasorOku
 import baslikOku
 import pdfOlustur
 
-class Arayuz:
-    def __init__(self, ana_pencere):
-        self.ana_pencere = ana_pencere
-        self.ana_pencere.title("PDF İşlemleri")
-        self.ana_pencere.geometry("300x200")
+def klasor_oku():
+    klasorOku.PDFIslemleri.klasor_oku()
 
-        self.menu_ogeleri = ["Pdf adlarını listele", "Pdf başlıklarını oku", "Başlıklardan yeni pdf oluştur", "Çıkış"]
-        self.menu_fonksiyonlari = [self.klasor_oku, self.pdf_oku, self.pdf_olustur, quit]
+def pdf_oku():
+    klasor_yolu = filedialog.askdirectory(title="Klasör Seç")
+    basliklar = baslikOku.PDFIslemleri.pdf_oku(klasor_yolu)
+    for baslik in basliklar:
+        print(baslik)
 
-        for idx, oge in enumerate(self.menu_ogeleri):
-            ttk.Button(self.ana_pencere, text=oge, command=self.menu_fonksiyonlari[idx]).pack()
+def pdf_olustur():
+    klasor_yolu = filedialog.askdirectory(title="Klasör Seç")
+    pdfOlustur.PDFIslemleri.pdf_olustur(klasor_yolu)
 
-    def klasor_oku(self):
-        klasorOku.PDFIslemleri.klasor_oku()
+pencere = Tk()
+pencere.title("PDF İşlemleri")
+pencere.geometry("300x200")
 
-    def pdf_oku(self):
-        klasor_yolu = filedialog.askdirectory(title="Klasör Seç")
-        basliklar = baslikOku.PDFIslemleri.pdf_oku(klasor_yolu)
-        for baslik in basliklar:
-            print(baslik)
+menuOgeleri = ["Pdf adlarını listele", "Pdf başlıklarını oku", "Başlıklardan yeni pdf oluştur", "Çıkış"]
+menuFonksiyonlari = [klasor_oku, pdf_oku, pdf_olustur, quit]
 
-    def pdf_olustur(self):
-        klasor_yolu = filedialog.askdirectory(title="Klasör Seç")
-        pdfOlustur.PDFIslemleri.pdf_olustur(klasor_yolu)
+for idx, oge in enumerate(menuOgeleri):
+    ttk.Button(pencere, text=oge, command=menuFonksiyonlari[idx]).pack()
 
-
-if __name__ == "__main__":
-    pencere = Tk()
-    arayuz = Arayuz(pencere)
-    pencere.mainloop()
+pencere.mainloop()
